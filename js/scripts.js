28d198c22db925f6619e6c15fc6fe8639d471ea9
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-03-04 23:38:21 +0100

    area of triangle

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// scripts.js file
+
+
+var a = prompt('Enter value a'); 
+	h = prompt ('Enter value h'); 
+
+var triangleArea = (a * h /2);
+
+alert('Triangle Area is: ' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
